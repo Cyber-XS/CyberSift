ef3519cf6d938db684f59a35bd1831d950596b0a
sonu kumar <166900890+Cyber-XS@users.noreply.github.com>
2025-10-26 10:39:36 +0530

    Delete debian.sh

--- debian.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update
-sudo apt install nmap
-sudo apt install subfinder
-sudo apt install amass
-sudo apt install dirb
-sudo apt install ffuf
-sudo apt install nuclei
-sudo apt install sublist3r
