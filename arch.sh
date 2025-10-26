6b5f8aff1d3d7036a0f752c6a4907e0740cde7ed
sonu kumar <166900890+Cyber-XS@users.noreply.github.com>
2025-10-26 10:39:57 +0530

    Add files via upload

---
+++ arch.sh
@@ -0,0 +1,9 @@
+sudo pacman -Sy
+sudo pacman -S nmap
+sudo pacman -S subfinder
+sudo pacman -S amass
+sudo pacman -S dirb
+sudo pacman -S ffuf
+sudo pacman -S nuclei
+sudo pacman -S sublist3r
+sudo pacman -S nikto
